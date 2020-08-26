53a7c277e0030a9e61e119ab3cf1c29b88a2c793
Ricardo Zandonai <zandonai.r@gmail.com>
2020-08-26 09:11:39 -0300

    init site
    site

---
+++ docs/precache-manifest.61a6ba42a3cfb52dbf78dc2de6e9c03d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4193551ef1c3f7063153222769e87d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4e5cc3cdf331c37782a",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "96543ca11cf760343467",
+    "url": "/static/js/2.fe61b73c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fe61b73c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4e5cc3cdf331c37782a",
+    "url": "/static/js/main.f60e67b6.chunk.js"
+  },
+  {
+    "revision": "97d2edfe6eb4d048b0e0",
+    "url": "/static/js/runtime-main.ec8369c9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
